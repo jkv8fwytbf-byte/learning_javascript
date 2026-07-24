cd46496e92483ed78953e204ea38fb52aaf1c5d4
siddharth <siddharth.muramkar1@gmail.com>
2026-07-24 09:33:22 +0530

    terniary fixed

--- 01-basics/operators/6.ternary.js
+++ 01-basics/operators/6.ternary.js
@@ -1,12 +1,7 @@
-// Ternary: short if/else that picks one of two values.
-// condition ? valueIfTrue : valueIfFalse
+//terniary or conditional operator
 
-const age = 20;
-const message = age >= 18 ? "adult" : "minor";
-console.log(message);
-
-const points = 110;
-const type = points > 100 ? "gold" : "silver";
+let points = 99;
+let type = points > 100 ? "gold" : "silver";
 console.log(type);
 
 // Try it:
