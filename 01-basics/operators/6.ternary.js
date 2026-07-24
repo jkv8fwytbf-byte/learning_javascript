2a0ebeb30273be6cafd03473aadb4e6563fdd1ef
siddharth <siddharth.muramkar1@gmail.com>
2026-07-24 09:40:18 +0530

    fixed

--- 01-basics/operators/6.ternary.js
+++
@@ -1,8 +0,0 @@
-// Ternary or conditional operator.
-
-let points = 99;
-let type = points > 100 ? "gold" : "silver";
-console.log(type);
-
-// Try it:
-// - Use a ternary to set canDrive based on age >= 16.
